502cf49d4cf0cc7f05c698c751e42ffdd3f322f3
root <root@localhost.localdomain>
2018-10-24 16:58:37 +0800

    1.1

--- install.sh
+++ install.sh
@@ -15,9 +15,9 @@ if [ "$OS" != "openmediavault" ];then
     exit
 fi
 if [ "$V" != "4" ];then
-	echo "您的Openmediavault版本不对，不建议安装！"
-	echo "Your Openmeidavalt's version is unmatched."
-	exit
+  echo "您的Openmediavault版本不对，不建议安装！"
+  echo "Your Openmeidavalt's version is unmatched."
+  exit
 fi
 if [ ! -d "$Omv" ];then
 	    echo "OMV路径不存在，无法安装。"
@@ -25,28 +25,28 @@ if [ ! -d "$Omv" ];then
 	    exit
 fi
 while [ true ]               
-		do
-		   echo -n "您的系统是：$OS $Version，OMV路径存在，是否进行安装?(y/n)"
-		   echo -n "Your OS：$OS $Version,continue?(y/n)"
-		   read x
-		   case "$x" in
-		     y | yes ) 
-			mv $Omv/css $Omv/css-blue
-			cp -R ./css-blue $Omv/css
-			cp -R ./images-blue $Omv/
-			chown -R openmediavault-webgui:openmediavault-webgui $Omv/css
-			chown -R openmediavault-webgui:openmediavault-webgui $Omv/images-blue
-			echo "安装完成，请刷新webui界面！"
-			echo "Installation Complete,please refresh your webui!"
-			exit
-			;;
+  do
+     echo -n "您的系统是：$OS $Version，OMV路径存在，是否进行安装?(y/n)"
+     echo -n "Your OS：$OS $Version,continue?(y/n)"
+     read x
+     case "$x" in
+       y | yes ) 
+    mv $Omv/css $Omv/css-blue
+    cp -R ./css-blue $Omv/css
+    cp -R ./images-blue $Omv/
+    chown -R openmediavault-webgui:openmediavault-webgui $Omv/css
+    chown -R openmediavault-webgui:openmediavault-webgui $Omv/images-blue
+    echo "安装完成，请刷新webui界面！"
+    echo "Installation Complete,please refresh your webui!"
+    exit
+    ;;
 
-		     n | no )
-			exit
-			;;
-		    
-		     * )
-		       echo "请输入y/n确认是否继续！"
-		       echo "Please input y/n to comfirm!"
-		   esac
+       n | no )
+    exit
+    ;;
+      
+       * )
+         echo "请输入y/n确认是否继续！"
+         echo "Please input y/n to comfirm!"
+     esac
 done
